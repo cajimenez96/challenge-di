0b13bc4f9fd1811f6e33c1e764a368f130bb5fbe
cajimenez96 <carlosj_utn@hotmail.com>
2022-11-08 19:15:11 -0300

    Update axios and create environment variable

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,21 +4,18 @@ import Card from '../Card/Card'
 const List = ({list, page}) => {
   return (
     <div className="d-flex justify-content-center flex-wrap">
-      { list &&
+      { list && page &&
           page === 'films'
-          ?  list.map((films, index) => {
-              return (
-                  <Card name={films.title} info={films.opening_crawl} page={page} key={index}/>
-              );
+        ?  list.map((films, index) => {
+            return (
+              <Card name={films.title} info={films.opening_crawl} page={page} key={index}/>
+            );
           })
-          
-          : page === 'people'
-          ?  list.map((people, index) => {
-              return (
-                <Card name={people.name} page={page}  key={index}/>
-              );
-            })
-          : <>Proximamente</>
+        : list.map((element, index) => {
+            return (
+              <Card name={element.name} page={page} key={index}/>
+            )
+          })
       }
     </div>
   )
--- src/pages/ViewElements/ViewElements.js
+++ src/pages/ViewElements/ViewElements.js
@@ -1,24 +1,48 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import Button from '../../components/Button/Button';
 import List from '../../components/List/List';
 import Spinner from '../../components/Spinner/Spinner';
-import { clientAxios } from '../../config/clientAxios';
+// import { clientAxios } from '../../config/clientAxios';
 
 const ViewElements = ({element}) => {
   const [listElements, setListElements] = useState();
-  const [nextPage, setNextPage] = useState();
-  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState();
+  const [loading, setLoading] = useState();
 
-  useEffect(() => {
-    clientAxios.get(`${element}`)
+  let baseUrl = process.env.REACT_APP_SWAPI_API_URL+element;
+
+  const getNewData = (baseUrl) => {
+    setLoading(true);
+    setListElements();
+    axios.get(baseUrl)
     .then(response => {
       setListElements(response.data.results);
-      setNextPage(response.data.next);
+      setPage(response.data.next);
+      setLoading(false);
     })
     .catch((err) => {
       console.log(err);
     })
     .finally(() => setLoading(!loading))
-  }, []);
+  };
+
+  useEffect(() => {
+    const getData = () => {
+      getNewData(baseUrl);
+    }
+    getData();
+  }, [element]);
+
+  const handleAddActors = async () => {
+    if (page) {
+      getNewData(page);
+    }
+  }
+
+  const handleScrollTop = () => {
+    window.scrollTo(0, 0)
+  }
 
   return (
     <>
@@ -26,6 +50,17 @@ const ViewElements = ({element}) => {
         listElements
         ? <>
             <List list={listElements} page={element} />
+            {
+              <div className="text-end w-75">
+                {listElements.length === 10
+                ? (
+                  <Button click={handleAddActors} text={'View more'}/>
+                )
+                : (
+                  <Button click={handleScrollTop} text={'Back to top'}/>
+                )}
+              </div>
+            }
           </>
         : <div className="vh-100 d-flex justify-content-center align-items-center">
             <Spinner status={loading} />
