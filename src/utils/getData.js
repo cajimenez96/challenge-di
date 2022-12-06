3cc0d31aa018ce22276b46bc86e38f03a930b81d
cjimenez96 <cjimenez@totalcoin.com>
2022-12-05 21:52:53 -0300

    Update order style modules

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,13 +1,14 @@
 import axios from "axios";
+
 //Conection to API
 const getData = async (url) => {
   try {
-    const res = await axios.get(url)
-    const result = await res.data
-    return result
+    const res = await axios.get(url);
+    const result = await res.data;
+    return result;
   } catch (err) {
-    if (axios.isCancel(err)) console.log('Failed to connect to server');
-    else console.log(err);
+    if (axios.isCancel(err)) console.error('Failed to connect to server');
+    else console.error(err);
   }
 }
 
